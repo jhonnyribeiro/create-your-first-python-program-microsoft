575b6c42056269f60090b1ae604862fbab83046a
Jhonny Ribeiro <jaribeiro32@gmail.com>
2021-12-16 18:19:49 -0300

    sum + type

---
+++ hello/numbers.py
@@ -0,0 +1,14 @@
+print("First Number:")
+first_number = input()
+print("Second Number:")
+second_number = input()
+sum = first_number + second_number
+print(sum)
+
+
+print("First Number:")
+first_number = int(input())
+print("Second Number:")
+second_number = int(input())
+sum = first_number + second_number
+print(sum)
